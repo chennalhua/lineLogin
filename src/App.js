f004277cf54b7d39e654a8292add0976296f463f
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2021-12-28 11:07:31 +0800

    update first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import AppRoute from './router/AppRoute';
+
+const App = () => {
+  return (
+    <>
+      <AppRoute />
+    </>
+  );
+}
+export default App;
---
+++ src/router/AppRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { HashRouter  as Router, Route, Redirect, NotFoundRoute } from "react-router-dom";
+import { BrowserRouter  as Router, Route, Redirect, NotFoundRoute } from "react-router-dom";
+
+import Login from "../view/Login"; //登入
+import Test from "../view/Test"; //登入
+
+const AppRoute = (props) => {
+    return (
+        <Router>
+            <Route exact path="/"><Login /></Route> {/* 登入 */}
+            <Route exact path="/qqq"><Test /></Route> {/* 登入 */}
+        </Router>
+    )
+}
+
+export default AppRoute
